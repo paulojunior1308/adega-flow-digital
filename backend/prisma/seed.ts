137aae5562fe64d338302c72118c1f04bd4ed547
Paulo Junior <pauloesjr2@gmail.com>
2025-05-27 10:26:53 -0300

    Sistema desenvolvido para delivery e controle de estoque

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -11,7 +11,7 @@ async function main() {
   console.log('Tabelas existentes no banco:', tables.map(t => t.table_name));
 
   // Apagar todas as tabelas do banco (exceto _prisma_migrations)
-await prisma.$executeRawUnsafe(`
+/*await prisma.$executeRawUnsafe(`
   DO $$ DECLARE
     r RECORD;
   BEGIN
@@ -21,7 +21,7 @@ await prisma.$executeRawUnsafe(`
   END $$;
 `);
 console.log('Tabelas apagadas com sucesso!');
-
+*/
   // Adicionar usuário admin
   await prisma.user.upsert({
     where: { email: 'pauloesjr2@gmail.com' },
@@ -35,7 +35,23 @@ console.log('Tabelas apagadas com sucesso!');
     },
   });
 
-
+  // Adicionar categorias
+  const categorias = [
+    'Whisky',
+    'Essências',
+    'GIN',
+    'Energéticos',
+    'Cervejas',
+    'Refrigerantes',
+    'Gelo de Coco',
+    'Sucos',
+    'Carvão',
+  ];
+  for (const name of categorias) {
+    await prisma.category.create({
+      data: { name, active: true },
+    });
+  }
 }
 
 main()
