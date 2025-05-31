ce42d0c3f760da8e52560977410ed33726e5b4ec
Paulo Junior <pauloesjr2@gmail.com>
2025-05-31 12:51:10 -0300

    Sistema desenvolvido para delivery e controle de estoque

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -20,7 +20,7 @@ async function main() {
     END LOOP;
   END $$;
 `);
-console.log('Tabelas apagadas com sucesso!');
+console.log('Tabelas apagadas com sucesso!');*/
 
   // Adicionar usuário admin
   await prisma.user.upsert({
@@ -53,7 +53,7 @@ console.log('Tabelas apagadas com sucesso!');
       data: { name, active: true },
     });
   }
-  console.log('Categorias adicionadas com sucesso!'+ prisma.category.findMany());*/
+  console.log('Categorias adicionadas com sucesso!'+ prisma.category.findMany());
 }
 
 main()
