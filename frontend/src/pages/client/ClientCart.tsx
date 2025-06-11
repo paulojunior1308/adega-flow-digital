b90cbb1e3add782d009708dca001d6e25f086a33
Paulo Junior <pauloesjr2@gmail.com>
2025-06-11 16:51:52 -0300

    Sistema desenvolvido para delivery e controle de estoque

--- frontend/src/pages/client/ClientCart.tsx
+++ frontend/src/pages/client/ClientCart.tsx
@@ -446,6 +446,7 @@ const ClientCart = () => {
                             <>
                               {/* Doses agrupadas */}
                               {Object.entries(doses).map(([doseName, items]) => {
+                                // O valor da dose é a soma dos preços proporcionais dos itens (que sempre soma o valor cadastrado)
                                 const doseTotal = items.reduce((sum, i) => sum + ((i.price ?? i.product.price) * i.quantity), 0);
                                 return (
                                   <div key={doseName} className="border rounded-md mb-4 bg-gray-50">
